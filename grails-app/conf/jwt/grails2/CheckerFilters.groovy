f3e8523226846b36317c8122bcceb4542bfe4aea
luizcintra <luizcintradev@gmail.com>
2025-03-23 18:01:54 -0300

    refactor: protejendo endpoint hello

--- grails-app/conf/jwt/grails2/CheckerFilters.groovy
+++ grails-app/conf/jwt/grails2/CheckerFilters.groovy
@@ -5,11 +5,21 @@ import org.apache.http.auth.AuthenticationException
 class CheckerFilters {
 
     def filters = {
-        loginCheck(controller:'login', action:'auth') {
+        loginCheck(controller: 'login', action: 'auth') {
             before = {
                 String secret = request.getHeader('secret_key')
                 if (secret != JWTManager.DEFAULT_SECRET) {
                     throw new AuthenticationException('Api não autenticada adicione a configuração correta')
+                }
+            }
+        }
+        authenticationCheck(controller: 'hello', action: '*') {
+            before = {
+                if (controllerName != 'login') {
+                    String bearerToken = request.getHeader('Authorization')
+                    String[] parts = bearerToken?.split('Bearer ')
+                    String jwt = parts && parts.size() == 2 ? parts[1] : null
+                    JWTManager.verifyAndDecode(jwt)
                 }
             }
         }
--- src/groovy/jwt/grails2/JWTManager.groovy
+++ src/groovy/jwt/grails2/JWTManager.groovy
@@ -4,6 +4,10 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.JWTCreator
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.exceptions.JWTVerificationException
+import com.auth0.jwt.interfaces.DecodedJWT
+
+import javax.naming.AuthenticationException
 
 class JWTManager {
 
@@ -13,7 +17,7 @@ class JWTManager {
     private static final Long THIRTY_SECONDS = 30_000L
     private static final Long EXPIRATION_TIME = 30 * MINUTE
     private static final Algorithm ALGORITHM = Algorithm.HMAC512(DEFAULT_SECRET)
-    private final JWTVerifier VERIFIER = JWT.require(ALGORITHM)
+    private static final JWTVerifier VERIFIER = JWT.require(ALGORITHM)
             .acceptLeeway(THIRTY_SECONDS)
             .build()
 
@@ -27,6 +31,18 @@ class JWTManager {
         builder.withSubject(UUID.randomUUID().toString())
                 .withClaim(USERNAME_FIELD, username)
                 .sign(ALGORITHM)
+    }
+
+    static DecodedJWT verifyAndDecode(String jwt) {
+        if (!jwt) {
+            throw new AuthenticationException('Token de autenticação faltando')
+        }
+
+        try {
+            return VERIFIER.verify(jwt)
+        } catch (JWTVerificationException ex) {
+            throw new AuthenticationException('Token de autenticação inválido')
+        }
     }
 
 }
